c82f1a9181735121d1f8c5d968db1d227ee3114f
Johnbel <johnbel@Johnbels-MacBook-Air.local>
2020-04-30 13:29:51 -0400

    upadate all files

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,104 @@
-function myFunction() {
-let d =1;
-let n=4;
-let z = d+n
-  document.getElementById("demo").innerHTML = z;
+//
+// function add() {
+//     var x = Number(document.getElementById('1').value);
+//     var y = Number(document.getElementById('2').value);
+//      var total = x+y;
+//     document.getElementById('result').innerHTML = total;
+//
+// }
+
+// var result = document.getElementById("result");
+
+// using annonymous fucntion storing id element from html
+var add = document.getElementById('add');
+add.addEventListener('click',() =>{
+  var num1 = parseInt(document.getElementById("num1").value);
+
+  var num2 = parseInt(document.getElementById("num2").value);
+
+  // console.log(num1);
+var total= num1 + num2;
+// console.log(total);
+var result = document.getElementById("result");
+
+
+// innerHTML
+result.innerHTML=total;
+
+});
+
+
+// using annonymous fucntion storing id element from html
+var multiply = document.getElementById('multiply');
+multiply.addEventListener('click',() =>{
+  var num1 = parseInt(document.getElementById("num1").value);
+
+  var num2 = parseInt(document.getElementById("num2").value);
+
+  // console.log(num1);
+var total= num1 * num2;
+// console.log(total);
+
+var result = document.getElementById("result");
+
+// innerHTML
+result.innerHTML=total;
+
+});
+
+
+// using annonymous fucntion storing id element from html
+var divide = document.getElementById('division');
+divide.addEventListener('click',() =>{
+  var num1 = parseInt(document.getElementById("num1").value);
+
+  var num2 = parseInt(document.getElementById("num2").value);
+
+  var total= num1 / num2;
+  // console.log(total);
+
+  var result = document.getElementById("result");
+
+  // innerHTML
+  // result.innerHTML=total;
+
+if(num1==0 || num2==0)
+{
+  console.log("NAN");
+  result.innerHTML=total;
+
+  document.getElementById("result").innerHTML = "nan";
+
 }
+else {
+  result.innerHTML=total;
+
+}
+  // console.log(num1);
+// var total= num1 / num2;
+// // console.log(total);
+//
+// var result = document.getElementById("result");
+//
+// // innerHTML
+// result.innerHTML=total;
+
+});
+
+
+
+// var multiply = document.getElementById('division');
+// add.addEventListener('click',() =>{
+//   var num1 = parseInt(document.getElementById("num1").value);
+//
+//   var num2 = parseInt(document.getElementById("num2").value);
+//
+//   // console.log(num1);
+// var total= num1 / num2;
+// // console.log(total);
+//
+//
+// // innerHTML
+// result.innerHTML=total;
+//
+// });
