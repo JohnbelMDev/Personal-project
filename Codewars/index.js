b0e86ad4d879e84c6dca88ee95dddbcefd51f338
Johnbel <johnbel@Johnbels-MacBook-Air.local>
2020-05-13 22:09:21 -0400

    Sum of two lowest positive integers begining example

---
+++ Codewars/index.js
@@ -0,0 +1,28 @@
+
+// let number =[2,4,44,25,11];
+
+// for(let i=0; i< number.lenght; i++)
+// {
+//     console.log(number[i]);
+//
+// }
+
+
+
+
+const number = [2, 4, 5,44,22,99];
+for (let i = 0; i < number.length; i++) {
+  //console.log(movies[i]);
+      var minimum
+   var j=5;
+  if(movies[i]<j)
+  {
+    console.log(number[i]);
+    var minimum = number[i];
+  console.log("minimum"+ minimum);
+  }
+
+   console.log("yo"+ minimum);
+
+
+}
