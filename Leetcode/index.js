9a421b9f563192063ad704a0dfea38df7e524f19
Johnbel <johnbel@Johnbels-MacBook-Air.local>
2020-05-15 23:27:45 -0400

    two sum indexes

---
+++ Leetcode/index.js
@@ -0,0 +1,11 @@
+let number = [2, 4, 5,44,22,99];
+let i = 0;
+//it's out because it change the value
+let total
+for (; i < number.length-4; i++) {
+  total= number[0] + number[1]
+
+
+}
+//reason why I'm printing it out side because it keept giving me the total twice
+console.log(total);
