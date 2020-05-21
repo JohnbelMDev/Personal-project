847f45496da9a1ce89c923b125428a750b1843b9
Johnbel <johnbel@Johnbels-MacBook-Air.local>
2020-05-20 23:34:27 -0400

    not finish

---
+++ Leetcode/Addtwonumbers/index.js
@@ -0,0 +1,16 @@
+//not yet finish all of which problems that I Have solved are solved without looking through hints nor online
+//I wil look online a bit later.
+
+
+
+console.log("This program takes an input and add two numbers")
+
+
+
+
+
+
+function addtwonumber(a, b)
+{
+  return a +b;
+}
